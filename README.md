61457c66e60cbe7b19a872b38d17ad7b7e0ee00f
Judy Lin <judith.s.lin@gmail.com>
2015-07-16 17:48:32 -0600

    completed

---
+++ README.md
@@ -0,0 +1,4 @@
+#Styleguide
+Goal: Utilize SASS in creating a styleguide.
+
+Powered By: NodeJS, Express, SASS, Reset, and TypeScale.
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('stylesheet');
+});
+
+router.get('/stylesheet', function(req, res, next) {
+  res.render('styleguide')
+});
+
+router.get('/post/new', function(req, res, next) {
+  res.render('index')
 });
 
 module.exports = router;
